638bd2118ed154982dd710a67803db07d6763985
Tsui Lin <10836024@ntub.edu.tw>
2020-02-04 22:53:33 +0800

    Create django-book-store

---
+++ books/views.py
@@ -0,0 +1,18 @@
+from rest_framework.decorators import action
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+
+from .models import Book
+from .serializers import BookSerializer
+
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.filter(is_online=True)
+    serializer_class = BookSerializer
+    permission_classes = []
+
+    @action(['get'], False, 'all', 'all')
+    def all_books(self, request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True) # many=True顯示多筆
+        return Response(serializer.data)
